63c32f17321437f4d096ec93367047d4c73deea5
Arxout <127612906+Arxout@users.noreply.github.com>
2023-10-22 18:08:54 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 9872472334 or game.PlaceId == 10324346056 or game.PlaceId == 10539706691 or game.PlaceId == 10324347967 or game.PlaceId == 10662542523 or game.PlaceId == 10808838353 or game.PlaceId == 11353528705 or game.PlaceId == 11353532384 or game.PlaceId == 12816512561  then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Arxout/WorxHub/main/Games/Evade/main.lua"))()
+end
